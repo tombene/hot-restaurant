b2e2cad538c65752ca5498b6c97eed997d1b3351
Thomas Benedict <tomtombene@gmail.com>
2018-04-19 20:02:33 -0600

    who knows

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # hot-restaurant
 
+##Web UI
+index page
+--------------
+Jumbotron -
+shows view tables
+make reservation
+links to API table and wait list -
+
+reserve page
+--------------
+Jumbotron - 
+veiw tables
+home
+table input-
+name, phone number, email, unique id
+submit button
+links to API table and wait list -
+
+waitlist page
+--------------
+Jumbotron -
+make reservation
+home
+current reservation table -
+displays incoming data
+waiting list -
+displays incoming data
+links to API table and wait list adn clear table option-
+
+#BackEnd
+
 ##Creat reservations
 array of reservations
 array of those on waiting list
@@ -7,6 +38,7 @@ array of those on waiting list
 After 5 reservations we add it to the waiting list
 
 ##Create server
+
 ###Dependencies Include
 NPM i express
 npm i body-parser
--- reservations.js
+++ reservations.js
@@ -15,79 +15,46 @@ app.use(bodyParser.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
-var characters = [
-  {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
-  }
-];
-
+var reservations = [{
+	customerName: "Bob",
+	customerPhone: "801.655.6555",
+	customerEmail: "bob@bob.com",
+	customerId: "uniqueid"
+}];
+var waitingList = [];
+var remaining = 5;
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+	res.sendFile(path.join(__dirname, "home.html"));
+	res.send("hello hot restaurant");
+
+
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+// make reservation
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-// Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
+// show reservatios
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
+// Displays all reservations
+app.get("/api/reservations", function(req, res) {
+  return res.json(reservations,waitingList);
 });
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body-parser middleware
-  var newcharacter = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newcharacter);
-
-  characters.push(newcharacter);
-
-  res.json(newcharacter);
+//Create New Reservations
+app.post("/api/reservations", function(req,res){
+	var newReservation = req.body;
+	console.log(newReservation);
+	// newReservation.customerName = 
 });
-
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
